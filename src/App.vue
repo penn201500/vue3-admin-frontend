634d281dbe6aad1a55b5e3df951fd1ceba6a4f91
penn <penn201500@gmail.com>
2024-11-07 23:47:13 +0800

    add footer component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
+import PageFooter from './components/PageFooter.vue'
 </script>
 
 <template>
   <RouterView />
+  <!-- footer section -->
+  <PageFooter />
 </template>
 
 <style lang="scss" scoped></style>
---
+++ src/components/PageFooter.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts" name="Header">
+// Get the current year dynamically
+const currentYear = new Date().getFullYear()
+</script>
+
+<template>
+  <footer class="bg-blue-800 text-gray-200 py-6 text-center">
+    <p>&copy; {{ currentYear }} Powered by Vue3. All rights reserved.</p>
+  </footer>
+</template>
