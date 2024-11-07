d2a90b824c0a37d67c90082ef201bf737238d88b
penn <penn201500@gmail.com>
2024-11-07 17:18:40 +0800

    fix: enable element-plus and register globally

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
-import './assets/main.css'
+import 'element-plus/dist/index.css'
+import ElementPlus from 'element-plus'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
@@ -10,5 +11,6 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(ElementPlus)
 
 app.mount('#app')
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,10 @@
+<script setup lang="ts"></script>
+
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div>
+    <el-button type="primary">Login</el-button>
+    <el-button type="success">Get User-info</el-button>
   </div>
 </template>
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>
+<style lang="scss" scoped></style>
