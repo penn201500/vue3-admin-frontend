279780c9cbb8a1c537de7634f3f33ac2d7a41ba0
penn <penn201500@gmail.com>
2024-11-11 22:28:50 +0800

    refractor: put cert and key files' path info into .env file

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,25 @@
 import { fileURLToPath, URL } from 'node:url'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import fs from 'fs'
 import path from 'path'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd())
+  return {
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
     },
-  },
-  server: {
-    https: {
-      key: fs.readFileSync(path.resolve(__dirname, '/Users/mac/tmp/localhost+2-key.pem')),
-      cert: fs.readFileSync(path.resolve(__dirname, '/Users/mac/tmp/localhost+2.pem')),
+    server: {
+      https: {
+        key: fs.readFileSync(path.resolve(__dirname, env.VITE_SSL_KEY_PATH)),
+        cert: fs.readFileSync(path.resolve(__dirname, env.VITE_SSL_CERT_PATH)),
+      },
+      port: 3000,
     },
-    port: 3000,
-  },
+  }
 })
