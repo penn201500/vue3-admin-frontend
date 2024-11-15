34e5fc3fb2d7aa732ebfbf5da3c6278eff010407
penn <penn201500@gmail.com>
2024-11-16 05:30:53 +0800

    feat: add logout function in HeaderComponent.vue

--- src/components/layout/HeaderComponent.vue
+++ src/components/layout/HeaderComponent.vue
@@ -26,16 +26,16 @@
         inline-prompt
         :active-icon="Sunrise"
         :inactive-icon="Moon"
-        size="medium"
+        size="default"
       />
       <!-- User Profile Dropdown -->
-      <el-dropdown>
+      <el-dropdown @command="handleCommand">
         <el-avatar :icon="UserFilled" class="cursor-pointer" size="small" />
         <template #dropdown>
           <el-dropdown-menu>
-            <el-dropdown-item>Profile</el-dropdown-item>
-            <el-dropdown-item>Settings</el-dropdown-item>
-            <el-dropdown-item>Logout</el-dropdown-item>
+            <el-dropdown-item command="Profile">Profile</el-dropdown-item>
+            <el-dropdown-item command="Settings">Settings</el-dropdown-item>
+            <el-dropdown-item command="Logout">Logout</el-dropdown-item>
           </el-dropdown-menu>
         </template>
       </el-dropdown>
@@ -49,14 +49,38 @@ import {
   ElInput,
   ElDropdown,
   ElAvatar,
-  ElIcon
+  ElIcon,
+  ElDropdownMenu,
+  ElDropdownItem,
+  ElSwitch,
 } from 'element-plus'
 import { Search, UserFilled, Moon, Sunrise } from '@element-plus/icons-vue'
 import { ref, computed } from 'vue'
 import { useThemeStore } from '@/stores/theme'
+import { useAuthStore } from '@/stores/authStore'
+import { useRouter } from 'vue-router'
 
+const authStore = useAuthStore()
+const router = useRouter()
 const searchText = ref('')
 const themeStore = useThemeStore()
 const isDarkMode = computed(() => themeStore.isDarkMode)
 const toggleTheme = () => themeStore.toggleDarkMode()
+
+const handleCommand = async (command: string) => {
+  switch (command) {
+    case 'Profile':
+      console.log('Command received:', command)
+      await router.push('/profile')
+      break
+    case 'Settings':
+      await router.push('/settings')
+      break
+    case 'Logout':
+      await authStore.logout()
+      break
+    default:
+      break
+  }
+}
 </script>
