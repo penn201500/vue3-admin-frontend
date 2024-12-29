013b37c1d1bdbbe78a7dcae49318507bbdebf2c5
penn <penn201500@gmail.com>
2024-12-29 12:25:43 +0800

    feat: add types for role

--- src/types/Role.ts
+++ src/types/Role.ts
@@ -2,4 +2,22 @@ export interface Role {
   id: number
   name: string
   code: string
+  status: number
+  is_system: boolean
+  is_active: boolean
+  create_time: string
+  update_time: string
+  remark: string | null
+}
+
+export interface RoleForm {
+  name: string
+  code: string
+  status: number
+  remark: string | null
+}
+
+export interface RoleQuery {
+  search?: string
+  ordering?: string
 }
