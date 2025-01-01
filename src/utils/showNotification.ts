f6b6805e9477a53db66056512c56bffdf78fd3ca
penn <penn201500@gmail.com>
2025-01-01 10:32:40 +0800

    feat: add cn and en mapping files and update showNotification.ts for multi-language switch

--- src/utils/showNotification.ts
+++ src/utils/showNotification.ts
@@ -1,10 +1,17 @@
-import { ElNotification } from 'element-plus';
+import { ElNotification } from 'element-plus'
+import { useI18n } from 'vue-i18n'
 
-export function showNotification(title: string, message: string, type: 'success' | 'error' | 'warning' | 'info') {
+export function showNotification(
+  title: string,
+  message: string,
+  type: 'success' | 'error' | 'warning' | 'info',
+) {
+  const { t } = useI18n()
+
   ElNotification({
-    title,
-    message,
+    title: t(`notifications.${type}.${title}`),
+    message: t(`notifications.${type}.${message}`),
     type,
     duration: 2000,
-  });
+  })
 }
